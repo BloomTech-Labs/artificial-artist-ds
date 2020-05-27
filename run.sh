8f1bc1088798af6ed09bdba87b4d668000048e9b
Jonathan Mendoza <jon_94us@yahoo.com>
2020-05-26 20:14:37 -0500

    Adding named groups for users to select and changing app route configuration for flask

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gunicorn -w 2 -t 3600 -b 0.0.0.0:5000 --access-logfile - --error-logfile - run:application
+exec gunicorn -w 4 -t 3600 -b 0.0.0.0:5000 --access-logfile - --error-logfile - run:application
