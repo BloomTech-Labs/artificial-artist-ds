4ba601e934f4d22e0b0638926ee56771799c1b5c
StevenMElliott <smonroeelliott@gmail.com>
2020-05-28 20:01:15 -0600

    updated vulpine to foxy, added classes argument

--- helper.py
+++ helper.py
@@ -2,16 +2,29 @@
 import requests
 from visualize import song_analysis, generate_images, save_video
 from flask import Response
+from image_groups import IMAGE_GROUPS
+import random
 
+def choose_classes(im_group):
+	if im_group == None:
+		im_group = random.sample(IMAGE_GROUPS.keys(), 1)
 
-def check_entry(preview, video_id, resolution):
+	if len(IMAGE_GROUPS[im_group[0]]) < 4:
+		im_classes = IMAGE_GROUPS[im_group[0]]
+	else:
+		im_classes = random.sample(IMAGE_GROUPS[im_group[0]], 4)
+	return im_classes
+
+def check_entry(preview, video_id, resolution, im_group):
 
 	r = requests.get(preview).status_code
 
 	vis_url = 'http://sample.eba-5jeurmbw.us-east-1.elasticbeanstalk.com/visualize'
 
+	classes = choose_classes(im_group)
+
 	data = {"preview": preview, "video_id": video_id,
-			"resolution": resolution}
+			"resolution": resolution, "classes": classes}
 
 	if r == 200:
 		try:
@@ -28,12 +41,12 @@ def generate_and_save(preview, video_id, resolution):
 
 	song = requests.get(preview)
 
-	open(f"{video_id}.mp3", 'wb').write(song.content)
+	open("song.mp3", 'wb').write(song.content)
 
-	noise_vectors, class_vectors = song_analysis(f"{video_id}.mp3")
+	noise_vectors, class_vectors = song_analysis("song.mp3")
 
 	frames = generate_images(noise_vectors, class_vectors, resolution)
 
-	save_video(frames, f"{video_id}.mp3", video_id)
+	save_video(frames, "song.mp3", video_id)
 
 	return 'Saved to S3'
